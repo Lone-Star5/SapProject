f1580d154b1133ff260ad4b7a0abeef1cfe3ca3f
Lone-Star5 <amankap05@gmail.com>
2020-11-21 19:00:02 +0530

    added functionality to navbar on manager page and added some frontend for reassigning tasks of sick employees

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const ejs = require('ejs');
 const app = express();
 const admin=require('firebase-admin');
-const serviceAccount=require('./sapproject-28227-firebase-adminsdk-hcmqw-6058543d99.json');
+// const serviceAccount=require('./sapproject-28227-firebase-adminsdk-hcmqw-6058543d99.json');
 
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount)
+//   });
 
-const db=admin.firestore();
+// const db=admin.firestore();
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 
@@ -22,7 +22,17 @@ app.get('/employee' , (req,res) => {
 })
 
 app.get('/manager', (req,res) => {
-    res.render('manager/manager');
+    tasks = [
+        {
+            name: 'Task 1',
+            Employee: ['Employee 1', 'Employee 2'],
+            points: 10,
+            deadline: '2020-12-06',
+            link: 'www.google.com',
+            description: 'This is Task 1'
+        }
+    ]
+    res.render('manager/manager',tasks=tasks);
 })
 
 app.listen('3000', ()=>{
