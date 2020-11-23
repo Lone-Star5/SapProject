47f46f9391b7140e206395a19cd6264216de9499
Arjun Khanduri <arjun99.khanduri@gmail.com>
2020-11-23 19:46:18 +0530

    Update app.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req,res) => {
-    res.send('working');
+    res.render('landing');
 })
 
 app.get('/manager', (req,res) => {
